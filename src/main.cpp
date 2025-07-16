c318f3d613e8486d811fac86a28d0e1f8e1d5075
Medhi Rishabh <rishabh.medhi@gmail.com>
2025-07-16 14:26:37 -0500

    Worked on average hash on images

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,81 @@
 #include <iostream>
 #include <opencv2/opencv.hpp>
 #include <filesystem>
+#include <thread>
+#include <vector>
+#include <mutex>
+
+#define DIM 128
 
 using namespace std;
 namespace fs = filesystem;
 
-int main() {
-	return 1;
+mutex sum_mutex;
+
+void add_pixels(const cv::Mat m, double& sum) {
+	double local_sum = 0;
+	switch(m.type()) {
+	case CV_8UC1:
+		for (int i = 0; i < m.cols; i += 1) {
+			local_sum += m.at<uchar>(0, i);
+		}
+		break;
+	case CV_32FC1:
+		for (int i = 0; i < m.cols; i += 1) {
+			local_sum += m.at<float>(0, i);
+		}
+		break;
+	default:
+		cerr << "Unsupported Mat type in add_pixels\n";
+		return;
+	}
+
+	lock_guard<mutex> lock(sum_mutex);
+	sum += local_sum;
+}
+
+int main(int argc, char *argv[]) {
+	if (argc < 3) {
+		cerr << "Usage: image_similarity <img1> <img2>";
+		return 1;
+	}
+
+	// Get image paths
+	fs::path path1(argv[1]);
+	fs::path path2(argv[2]);
+	string strpath1 = path1.string();
+	string strpath2 = path2.string();
+
+	// Load images
+	cv::Mat img1 = cv::imread(strpath1);
+	cv::Mat img2 = cv::imread(strpath2);
+
+	// Resize to 8x8
+	cv::Mat img1_8, img2_8;
+	cv::resize(img1, img1_8, cv::Size(DIM, DIM));
+	cv::resize(img2, img2_8, cv::Size(DIM, DIM));
+
+	// Convert to grayscale
+	cv::Mat gray1, gray2;
+	cv::cvtColor(img1_8, gray1, cv::COLOR_BGR2GRAY);
+	cv::cvtColor(img2_8, gray2, cv::COLOR_BGR2GRAY);
+
+	double sum1 = 0, sum2 = 0;
+	vector<thread> threads;
+	for (int r = 0; r < DIM; r += 1) {
+		threads.emplace_back(thread(add_pixels, gray1.row(r), std::ref(sum1)));
+		threads.emplace_back(thread(add_pixels, gray2.row(r), std::ref(sum2)));
+	}
+
+	for (auto& thread : threads) {
+		thread.join();
+	}
+
+	//cv::Mat img1_test;
+	//cv::resize(img1, img1_test, cv::Size(img1.cols / 2, img1.rows / 2));
+	cout << gray1.rows << " " << gray1.cols << endl;
+	cv::imshow("img 1", gray1);
+	cv::waitKey(0);
+
+	return 0;
 }
