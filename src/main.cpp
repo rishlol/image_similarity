ba00d299b7eeb7de9bb21bbb2e328bb7f9a9d472
rish <rishabh.medhi@gmail.com>
2025-07-16 20:18:04 -0500

    Use hash-based similarity checker

--- src/main.cpp
+++ src/main.cpp
@@ -3,59 +3,44 @@
 #include <filesystem>
 #include <thread>
 #include <vector>
-#include <mutex>
+#include <cstdint>
 
 using namespace std;
 namespace fs = filesystem;
 
 constexpr auto DIM = 8;
-mutex sum_mutex;
-
-void add_pixels(const cv::Mat m, double& sum) {
-	double local_sum = 0;
-	switch(m.type()) {
-	case CV_8UC1:
-		for (int i = 0; i < m.cols; i += 1) {
-			local_sum += m.at<uchar>(0, i);
-		}
-		break;
-	case CV_32FC1:
-		for (int i = 0; i < m.cols; i += 1) {
-			local_sum += m.at<float>(0, i);
-		}
-		break;
-	default:
-		cerr << "Unsupported Mat type in add_pixels\n";
-		return;
-	}
-
-	lock_guard<mutex> lock(sum_mutex);
-	sum += local_sum;
-}
-
-void threshold(cv::Mat& m, const double avg) {
-	for (int r = 0; r < m.rows; r += 1) {
-		for (int c = 0; c < m.cols; c += 1) {
-			switch (m.type()) {
-			case CV_8UC1:
-				m.at<uchar>(r, c) = m.at<uchar>(r, c) >= avg ? 255 : 0;
-				break;
-			case CV_32FC1:
-				m.at<float>(r, c) = m.at<float>(r, c) >= avg ? 1.0 : 0;
-				break;
-			default:
-				cerr << "Unsupported Mat type in threshold\n";
-				return;
-			}
-		}
-	}
-}
 
 cv::Mat normalize_image(cv::Mat img) {
-	cv::Mat out;
-	cv::normalize(img, out, 0.0, 1.0, cv::NORM_MINMAX);
-	out.convertTo(img, CV_8U, 255.0);
-	return img;
+    cv::Mat out;
+    cv::normalize(img, out, 0.0, 1.0, cv::NORM_MINMAX);
+    out.convertTo(img, CV_8U, 255.0);
+    return img;
+}
+
+uint64_t average_hash(const cv::Mat &img) {
+    // Expects img to be of type CV_8UC1
+    double avg = cv::mean(img)[0];
+    
+    // Build 64-bit hash
+    uint64_t hash = 0;
+    for (int r = 0; r < DIM; r += 1) {
+        for (int c = 0; c < DIM; c += 1) {
+            hash <<= 1;
+            if (img.at<uchar>(r, c) >= avg) {
+                hash |= 1;
+            }
+        }
+    }
+    return hash;
+}
+
+int hamming_distance(uint64_t diff) {
+    int count = 0;
+    while(diff > 0) {
+        count += diff & 1;
+        diff >>= 1;
+    }
+    return count;
 }
 
 int main(int argc, char *argv[]) {
@@ -83,54 +68,33 @@ int main(int argc, char *argv[]) {
 	cv::Mat gray1, gray2;
 	cv::cvtColor(img1_8, gray1, cv::COLOR_BGR2GRAY);
 	cv::cvtColor(img2_8, gray2, cv::COLOR_BGR2GRAY);
-
-	// Get averages
-	double sum1 = 0, sum2 = 0;
-	vector<thread> threads;
-	for (int r = 0; r < DIM; r += 1) {
-		threads.emplace_back(thread(add_pixels, gray1.row(r), std::ref(sum1)));
-		threads.emplace_back(thread(add_pixels, gray2.row(r), std::ref(sum2)));
-	}
-
-	for (auto& thread : threads) {
-		thread.join();
-	}
-	double avg1 = sum1 / gray1.total(), avg2 = sum2 / gray2.total();
-
-	// Normalize images if necessary
-	if (gray1.type() == CV_8UC1) {
-		gray1 = normalize_image(gray1);
-	}
-	if (gray2.type() == CV_8UC1) {
-		gray2 = normalize_image(gray2);
-	}
-
-	// Threshold
-	thread t1 = thread(threshold, std::ref(gray1), avg1);
-	thread t2 = thread(threshold, std::ref(gray2), avg2);
-	t1.join();
-	t2.join();
+    
+    // Normalize images if necessary
+    if (gray1.type() == CV_8UC1) {
+        gray1 = normalize_image(gray1);
+    }
+    if (gray2.type() == CV_8UC1) {
+        gray2 = normalize_image(gray2);
+    }
+    
+    // Get hashes
+    uint64_t hash1 = average_hash(gray1), hash2 = average_hash(gray2);
+    uint64_t comp = hash1 ^ hash2;
 
 	// Compare images
-	cv::Mat res(DIM, DIM, CV_8UC1);
-	cv::compare(gray1, gray2, res, cv::CMP_NE);
-	int ne = cv::countNonZero(res);
-	bool similar = ne == 0;
-
+    int ne = hamming_distance(comp);
+    bool similar = ne == 0;
+    
+//    cout << "Difference " << ne << endl;
 	if (similar) {
 		cout << strpath1 << " and " << strpath2 << " are similar!\n";
 	} else {
 		cout << strpath1 << " and " << strpath2 << " are NOT similar\n";
 	}
 
-	//cv::Mat img1_test;
-	//cv::resize(img1, img1_test, cv::Size(img1.cols / 2, img1.rows / 2));
-	//cout << gray1.rows << " " << gray1.cols << endl;
-	//cout << avg1 << " " << avg2 << endl;
-	//cout << similar << endl;
-	//cv::imshow("img 1", gray1);
-	//cv::imshow("img 2", gray2);
-	//cv::waitKey(0);
+//	cv::imshow("img 1", gray1);
+//	cv::imshow("img 2", gray2);
+//	cv::waitKey(0);
 
 	return 0;
 }
